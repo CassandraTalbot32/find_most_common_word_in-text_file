00fda3efeb00cc434b403ae1c0c042db494f978d
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2022-10-20 13:08:39 +0100

    Update most_common_txt.py

--- most_common_txt.py
+++ most_common_txt.py
@@ -1,7 +1,9 @@
 import re
-
 from collections import Counter
 
+# Open file and use cointer to find most common word
 words = findall(r'\w+', open('filename.txt').read().lower())
 count = Counter(words).most_common(15)
+
+# Output most common word 
 print(count)
